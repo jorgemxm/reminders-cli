ae79990d17a373c036f42bcfe5c834f1620bc266
ian <k>
2020-05-23 12:01:24 -0500

    feat: UpdateNotifier config based on latest version of NPM Pkg
    Check for Updates every 30 days.
    Pending to review if the interval should be smaller.

--- src/utils/updateNotifier.js
+++ src/utils/updateNotifier.js
@@ -4,34 +4,38 @@ import chalk from 'chalk';
 
 import pkg from '../../package.json';
 
-export default () => {
-  updateNotifier({
+export function updateNotify() {
+  const notifier = updateNotifier({
     pkg,
-    callback: (data, update) => {
-      if (!update || pkg.version === update.latest) {
-        return;
-      }
+    updateCheckInterval: 1000 * 60 * 60 * 24 * 30, // 30 days
+  });
 
-      const message = chalk.cyan(
-        `There's an update of ${pkg.name} available:`,
+  if (notifier.update) {
+    const { update: { latest = '' } = {} } = notifier;
+
+    if (pkg.version === latest) {
+      return;
+    }
+
+    const message = chalk.cyan(
+      `There's an update of ${pkg.name} available:`,
       chalk.dim(pkg.version),
       chalk.reset('→'),
       chalk.blue(latest),
       chalk.dim('\nRun'),
       chalk.green(chalk.bold(`npm i -g ${pkg.name}`)),
       chalk.dim('to update.')
-      );
+    );
 
-      const boxenOptions = {
-        padding: 1,
-        borderColor: 'blue',
-        dimBorder: true,
-        float: 'center',
-        align: 'center',
-        margin: 1,
-      };
+    const boxenOptions = {
+      padding: 1,
+      borderColor: 'blue',
+      dimBorder: true,
+      float: 'center',
+      align: 'center',
+      margin: 1,
+    };
 
-      console.log(boxen(message, boxenOptions));
-    },
-  });
-};
+    console.log(boxen(message, boxenOptions));
+  }
+}
