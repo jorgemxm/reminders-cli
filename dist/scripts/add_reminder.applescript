c0a7dd7ebea49e1831aba379b6a1a9fb6e96bf39
ian <k>
2020-05-23 10:03:35 -0500

    [v0.2.0] Quick Fix for OSX Catalina

--- dist/scripts/add_reminder.applescript
+++ dist/scripts/add_reminder.applescript
@@ -9,7 +9,8 @@
 on run argv
     set dateString to date (item 4 of argv)
     tell application "Reminders"
-        -- make new reminder with properties { name:"Title", remind me date:date "Sunday, May 6, 2018 at 2:00:00 PM" }
+        -- make new reminder with properties { name:"Title", remind me date:date "Friday, June 5, 2020 at 2:00:00 PM" }
+        -- make new reminder with properties { name:"Title", due date:date "Friday, 5 June 2020, 3:00:00 PM" }
         make new reminder with properties { name: item 1 of argv, remind me date: dateString }
         -- quit
     end tell
--- src/commands/add.js
+++ src/commands/add.js
@@ -3,6 +3,7 @@ import moment from 'moment';
 import chalk from 'chalk';
 import ora from 'ora';
 import inquirer from 'inquirer';
+import os from 'os';
 import { DateRange } from '../utils';
 
 const tic = chalk.green('✓');
@@ -11,6 +12,10 @@ const tac = chalk.red('✗');
 const spinner = ora();
 
 const scriptPath = `${__dirname}/../scripts/add_reminder.applescript`;
+
+const version = os.release();
+const osVersion = version.split('.').shift();
+const isCatalina = osVersion >= 19;
 
 const addReminder = async (providedArgs = {}) => {
   let info = providedArgs;
@@ -78,17 +83,20 @@ const addReminder = async (providedArgs = {}) => {
 
     // moment('12/25/2018-14:30', "MM/DD/YYYY-HH:mm");
     reminderDate = moment(`${info.date}-${info.time}`, 'DD/MM/YYYY-HH:mm');
-    // console.log('[DEBUG:date]', info.date);
 
-    // Date Format for AppleScript: "Tuesday, Dec 25, 2018 at 02:30:00 PM"
-    info.appleDate = reminderDate.format('dddd, MMM DD, gggg [at] hh:mm:ss A');
+    if (isCatalina) {
+      // Date Format for AppleScript: "Friday, 5 June 2020, 3:00:00 PM"
+      info.appleDate = reminderDate.format('dddd, DD MMM gggg, hh:mm:ss A');
+    } else {
+      // Date Format for AppleScript NOT Catalina: "Tuesday, Dec 25, 2018 at 02:30:00 PM"
+      info.appleDate = reminderDate.format('dddd, MMM DD, gggg [at] hh:mm:ss A');
+    }
 
     await applescript.execFile(scriptPath, Object.values(info));
 
     spinner.stop();
 
     console.log(`${tic} Reminder created successfully!`);
-    // console.log(`${tic} ${info.name} on ${info.date} at ${info.time}.`);
     console.log(`${tic} ${info.name} on ${info.appleDate}.`);
   } catch (err) {
     spinner.stop();
